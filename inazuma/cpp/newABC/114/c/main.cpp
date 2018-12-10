b8869345760aa5cfbdfe9aa21119e26126b4096c
Inazuma110 <c011703534@edu.teu.ac.jp>
2018-12-10 15:11:33 +0900

    114C

--- inazuma/cpp/newABC/114/c/main.cpp
+++ inazuma/cpp/newABC/114/c/main.cpp
@@ -11,14 +11,32 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> p;
 
-int main(){
-  int n;
-  cin >> n;
-  map<int, int> mp;
-  int digit = log10(n) + 1;
-  for (int i = 0; i < digit; i++) {
-    mp[3] = (double(n) % pow(10, i) >= 3) ? 1 : 0;
+int n;
+
+int res = 0;
+
+void dfs(string pos)
+{
+  if(stoll(pos) > n) return;
+  bool three = false, five = false, seven = false;
+  for (int i = 0; i < int(pos.size()); i++)
+  {
+    if(pos[i] == '3') three = true;
+    if(pos[i] == '5') five = true;
+    if(pos[i] == '7') seven = true;
   }
+  if(three && five && seven) res++;
+  dfs(pos + "3");
+  dfs(pos + "5");
+  dfs(pos + "7");
+}
+
+int main(){
+  cin >> n;
+  dfs("3");
+  dfs("5");
+  dfs("7");
+  cout << res << endl;
 
 
 }
