a67deae803d3afb029e15ca528146d0e142b6b6c
Inazuma110 <c011703534@edu.teu.ac.jp>
2019-01-17 21:28:34 +0900

    keyence

---
+++ inazuma/cpp/newABC/051/d/main.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#if __has_include("print.hpp")
+  #include "print.hpp"
+#endif
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+typedef long long ll;
+typedef pair<int, int> p;
+
+struct bridge {
+  int to;
+  int cost;
+  bool isUsed = false;
+};
+
+int res = 0;
+
+int main(){
+  int n, m;
+  cin >> n >> m;
+  map<int, bridge> mp;
+
+  for (int i = 0; i < n; i++)
+
+  }
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+    }
+  }
+
+
+}
